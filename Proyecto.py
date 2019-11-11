76345b46ebd173e8e5daee7a6bfa1a28493a744f
Paolo Galiano <paologaliano7@gmail.com>
2019-11-11 08:28:49 -0500

    Evité todo lo que dijo Aldito

--- Proyecto.py
+++ Proyecto.py
@@ -1,31 +1,42 @@
 #Version 1.3
 
-presupuesto = float(input("Ingrese preupuesto para materiales de construcción:"))
+# Registro de presupuesto
 
-listado_productos = [["Fierro_un_medio",24.47,"varilla"],["Fierro_tres_octavos",13.84,"varilla"],["Fierro_cinco_octavos",37.68,"varilla"],["alambre_negro",3.48,"kg"],["Clavos",3.87,"kg"],["Cemento_portland",21.18,"bolsa"],["Cal",22.03,"bolsa"],["Yeso",7.13,"bolsa"],["Madera",5.9,"pie_cuadrado"],["Triplay",36.82,"plancha"],["Arena_Gruesa",50.41,"metro_cubico"],["Hormigon",50.88,"metro_cubico"],["Mayólica",20.78,"metro_cuadrado"],["Piedra_chancada",59.37,"metro_cubico"],["ladrillo_king_kong",672.53,"millar"],["ladrillo_pandereta",557.5,"millar"],["ladrillo_techo",2059.35,"millar"],["tubo_PVC_electricidad",2.64,"tubo"],["tubo_PVC_desasgüe",10.06,"tubo"],["alambre_TW12",1.85,"metro"],["alambre_TW14",1.23,"metro"],["cable_TW6",3.69,"metro"],["cable_TW8",5.61,"metro"],["alambre_y_cables_inst_tel",1.57,"metro"]]
+presupuesto = float(input("Ingrese presupuesto del proyecto con respecto a los materiales de construcción: "))
+
+# Materiales organizados en lista
+
+listado_productos = [["Fierro_un_medio",24.47,"Varilla"],["Fierro_tres_octavos",13.84,"Varilla"],["Fierro_cinco_octavos",37.68,"Varilla"],["Alambre_negro",3.48,"kg"],["Clavos",3.87,"kg"],["Cemento_portland",21.18,"Bolsa"],["Cal",22.03,"Bolsa"],["Yeso",7.13,"Bolsa"],["Madera",5.9,"Pie_cuadrado"],["Triplay",36.82,"Plancha"],["Arena_gruesa",50.41,"Metro_cúbico"],["Hormigón",50.88,"Metro_cúbico"],["Mayólica",20.78,"Metro_cuadrado"],["Piedra_chancada",59.37,"Metro_cúbico"],["Ladrillo_king_kong",672.53,"Millar"],["Ladrillo_pandereta",557.5,"Millar"],["Ladrillo_techo",2059.35,"Millar"],["Tubo_PVC_electricidad",2.64,"Tubo"],["Tubo_PVC_desasgüe",10.06,"Tubo"],["Alambre_TW12",1.85,"Metro"],["Alambre_TW14",1.23,"Metro"],["Cable_TW6",3.69,"Metro"],["Cable_TW8",5.61,"Metro"],["Alambre_y_cables_instalación_telefónica",1.57,"Metro"]]
 
 costo = 0
 
 for i in range(len(listado_productos)):
-    print(i+1,listado_productos[i])
+    print(i+1, listado_productos[i])
+
+# Registro del material requerido (tipo, cantidad y costo)
 
 while True:
     while True:
-        pos = int(input("Ingrese la posición del producto, iniciando de 1 a 24, ingrese 0 para finalizar:"))
+        pos = int(input("Ingrese la posición del producto, iniciando de 1 a 24; digite 0 para finalizar: "))
         if pos >= 0 and pos <= len(listado_productos):
             break
     if pos == 0:
         break
-    cantidad = int(input("ingrese cantidad del producto seleccionado:"))
+    cantidad = int(input("Ingrese cantidad del producto seleccionado: "))
     costo = costo + (listado_productos[pos-1][1]*cantidad)
 
-print(costo)
+# Round para evitar el error de decimales con los múltiplos de 3
+
+costo = round(costo,2)
+
+print("El costo total de los materiales registrados es de", costo, "soles.")
+
+# Verificación de la viabilidad del proyecto
 
 viabilidad = presupuesto - costo
 if viabilidad > 0:
-    print("Es viable. Le sobran", viabilidad, "soles.")
+    print("El proyecto es viable. Su presupuesto excede en", viabilidad, "soles.")
 elif viabilidad == 0:
-    print("Es viable. Sin embargo, le sobran", viabilidad, "soles.")
+    print("El proyecto es viable. Sin embargo, su presupuesto es exacto; le quedan", viabilidad, "soles.")
 else:
-    print("No es viable. Le faltan", viabilidad*(-1), "soles.")
-
+    print("El proyecto no es viable. El costo de materiales excede a su presupuesto en", viabilidad*(-1), "soles.")
